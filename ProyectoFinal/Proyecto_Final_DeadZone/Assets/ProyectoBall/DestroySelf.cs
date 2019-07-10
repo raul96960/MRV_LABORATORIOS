b2423a69f999de5d5bd1a4dae9c273398781807a
raul <raul96.960@gmail.com>
2019-07-09 20:45:09 -0500

    Proyecto Final MRV_DeadZone

--- ProyectoFinal/Proyecto_Final_DeadZone/Assets/ProyectoBall/DestroySelf.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class DestroySelf : MonoBehaviour {
-	void Update () {
-		if (transform.position.y < -5f) {
-			Destroy (gameObject);
-		}
-	}
-}
